0a0fc5d4f0eb7f4c66e6c610c14f8f93655ddbc0
Mr Dipayan Dey <161328487+Dipayan-Dey@users.noreply.github.com>
2025-08-29 03:14:12 -0700

    Update Footer.jsx

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-gray-100 text-gray-600 py-4 mt-auto bottom-0">
       <div className="container mx-auto px-4 text-center">
         <p className="text-sm text-black">
-          &copy; {new Date().getFullYear()} TaskFlow. All rights reserved. Developed By <a target='_blank' href="https://dipayan-dey.github.io/portfolio/">Mr. Dipayan Dey</a>
+          &copy; {new Date().getFullYear()} TaskFlow. All rights reserved. Developed By <a className="underline" target='_blank' href="https://dipayan-dey.github.io/portfolio/">Mr. Dipayan Dey</a>
         </p>
       </div>
     </footer>
